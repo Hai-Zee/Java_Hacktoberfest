f9f32c3e32f6afe3d790ec8bd366b9842fafe97c
Hai-Zee <72437004+Hai-Zee@users.noreply.github.com>
2020-10-16 19:28:08 +0530

    Update to generate random number from given array.

--- src/Main.java
+++ src/Main.java
@@ -3,24 +3,15 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static Random random = new Random();
-    public static Scanner sc = new Scanner(System.in);
-
     public static void main(String args[]){
+        
+//         this program is to generate the random number.
+        
         int[] numbers = new int[]{5,4,8,9,6,3,7,4,2,85,63,45,10};
-        int ranNum = random.nextInt(numbers.length-1);
-        int a = numbers[ranNum];
-        System.out.print("Your generated random number is:  ");
-        System.out.println(a);
-        System.out.println("Enter a number and I will generate a random number in its range");
-        int max = sc.nextInt();
-        System.out.println("Random is " + max);
-        System.out.println("Sum of two random numbers generated from it");
-        System.out.println(sum(max));
-    }
-
-    public static int sum(int maxNum){
-        int a = random.nextInt(maxNum), b = random.nextInt(maxNum);
-        return a + b ;	
+        Random randomNum = new Random();
+        int num = randomNum.nextInt(numbers.length);
+        
+        System.out.println(numbers[num]);
+        
     }
 }
